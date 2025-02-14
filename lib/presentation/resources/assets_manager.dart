b13312bcd300cba4544c63226f789a1bcda65041
Allen Masud <100032855+allenmasud12@users.noreply.github.com>
2025-02-14 20:49:45 +0600

    feat:🎈assets manager

---
+++ lib/presentation/resources/assets_manager.dart
@@ -0,0 +1,6 @@
+const String IMAGE_PATH = "assets/images";
+
+class ImageAssets{
+  static const String appLogo = "$IMAGE_PATH/main_icon.png";
+  static const String quiz = "$IMAGE_PATH/quiz.png";
+}
