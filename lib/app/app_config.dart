18efc951391467e6820ec398563d5f73fa93c0d0
Allen Masud <100032855+allenmasud12@users.noreply.github.com>
2025-02-25 14:40:30 +0600

    feat:🎈firebase added

---
+++ lib/app/app_config.dart
@@ -0,0 +1,5 @@
+class AppConfig {
+  static const bool enableFirebase = false;
+
+  static const String androidFirebaseJsonPath = "assets/firebase/android.json";
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:dictionary/presentation/controller/dictionary_controller.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'app/app.dart';
+import 'app/app_config.dart';
 import 'app/controller/theme_controller.dart';
 import 'data/datasources/local/database_helper.dart';
 import 'data/repositories/dictionary_repository.dart';
@@ -10,6 +12,10 @@ import 'domain/usecases/get_words_usecase.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+
+  if (AppConfig.enableFirebase) {
+    await Firebase.initializeApp();
+  }
   await DatabaseHelper.instance.initializeDatabase();
 
   Get.put(DatabaseHelper.instance);
