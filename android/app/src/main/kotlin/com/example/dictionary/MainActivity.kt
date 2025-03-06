c8c124b01d116c858b660b6c552f382ac93c166d
Allen Masud <100032855+allenmasud12@users.noreply.github.com>
2025-03-06 16:06:30 +0600

    feat:🎈change package name

--- android/app/src/main/kotlin/com/example/dictionary/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/dictionary/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.dictionary
+package com.codecanyon.dictionary
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/app/app_config.dart
+++ lib/app/app_config.dart
@@ -1,5 +1,3 @@
 class AppConfig {
   static const bool enableFirebase = false;
-
-  static const String androidFirebaseJsonPath = "assets/firebase/android.json";
 }
